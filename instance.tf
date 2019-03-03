ce2e3577b1302424c08cddb2f88a05d4ec437e13
Aliya <aliyaibraimova@Aliyas-MacBook-Air.local>
2019-03-03 13:12:06 -0800

    instance added

---
+++ instance.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "example" {
+    ami = "${var.ami}"
+    instance_type = "${var.instance_type}"
+    key_name = "${var.keyname}"
+    subnet_id = "${var.subnet_id}"
+
+  
+}
---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "ami" {}
+
+variable "instance_type" {}
+
+variable "keyname" {}
+
+variable "Name" {}
+
+variable "subnet_id" {}
+
+
+
