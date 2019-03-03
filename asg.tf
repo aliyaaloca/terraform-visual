df8e8b91941a3d2a72d34e48108b6ca771f797be
Aliya <aliyaibraimova@Aliyas-MacBook-Air.local>
2019-03-03 14:14:21 -0800

    az added

--- asg.tf
+++ asg.tf
@@ -3,4 +3,5 @@ resource "aws_autoscaling_group" "dev" {
     launch_configuration = "${aws_launch_configuration.dev.name}"
     min_size = "${var.min_size}"
     max_size = "${var.max_size}" 
+    availability_zones = ["eu-west-1b", "eu-west-1a", "eu-west-1c"]
 }    
