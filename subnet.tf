ed30b13e8cc96261f303898364424d6b799e4fbd
Aliya <aliyaibraimova@Aliyas-MacBook-Air.local>
2019-03-06 16:16:34 -0800

    subnet added

--- subnet.tf
+++ subnet.tf
@@ -1,7 +1,9 @@
 resource "aws_subnet" "dev1" {
     vpc_id = "${aws_vpc.dev.id}"
-    cidr_block = "10.0.0.0/24"
-    
+    cidr_block = "10.0.1.0/24"
+}
 
-   
+resource "aws_subnet" "dev2" {
+    vpc_id = "${aws_vpc.dev.id}"
+    cidr_block = "10.0.2.0/24"
 }
