630fa38730c966485baaaffa52f7a0b9a9bcd51b
Aliya <aliyaibraimova@Aliyas-MacBook-Air.local>
2019-03-06 16:31:38 -0800

    asg

--- asg.tf
+++ asg.tf
@@ -3,5 +3,6 @@ resource "aws_autoscaling_group" "dev" {
     launch_configuration = "${aws_launch_configuration.dev.name}"
     min_size = "${var.min_size}"
     max_size = "${var.max_size}" 
-    availability_zones = ["eu-west-1b", "eu-west-1a", "eu-west-1c"]
+    vpc_zone_identifier = ["${aws_subnet.dev1.id}", "${aws_subnet.dev2.id}"]
+
 }    
